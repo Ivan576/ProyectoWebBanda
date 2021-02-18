57e09fa9e9af203d8aaec0f34efe889087e5b163
Ivan576 <zavalai576@gmail.com>
2021-02-17 20:33:26 -0600

    Agregue las referencia del maySicuel

--- ProyectoWebBanda/CapaDatos/DaoNoticias.cs
+++ ProyectoWebBanda/CapaDatos/DaoNoticias.cs
@@ -7,5 +7,18 @@ namespace ProyectoWebBanda.CapaDatos
 {
     public class DaoNoticias
     {
+        MySqlConnection conex = new MySqlConnection();
+        public void conexion()
+        {
+            try
+            {
+                conex.ConnectionString = "server=localhost;database=prograweb2;user id =root;password=root1;";
+                conex.Open();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
     }
 }
