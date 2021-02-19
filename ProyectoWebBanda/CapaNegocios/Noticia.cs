2453cceff00cd21a376e57a33e614e6446009b10
Batishavo <45341513+Batishavo@users.noreply.github.com>
2021-02-18 23:04:23 -0600

    Los objetos de Noticia y viculo

--- ProyectoWebBanda/CapaNegocios/Noticia.cs
+++ ProyectoWebBanda/CapaNegocios/Noticia.cs
@@ -7,5 +7,26 @@ namespace ProyectoWebBanda.CapaNegocios
 {
     public class Noticia
     {
+        public int idNoticia {get;set};
+        public String titulo {get;set;};
+        public String resumen {get;set;}
+        public String contenido{get;set;} 
+        public DateTime fecha {get;set;}
+        public String imagenPortada {get;set;}
+        public String imagenesContenido {get;set;}
+
+        public Noticia(String titulo,String resumen,String contenido,DateTime fecha,String imagenPortada,String imagenesContenido){
+            
+            this.titulo=titulo;
+            this.resumen=resumen;
+            this.contenido=contenido;
+            this.fecha=fecha;
+            this.imagenPortada=imagenPortada;
+            this.imagenesContenido=imagenesContenido;
+ 
+        }
+        public Noticia(int idNoticia){
+            this.idNoticia=idNoticia;
+        }
     }
 }
