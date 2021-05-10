de9ea470aae0d61520890433a46083a1317427e4
Carlos <Carlos@DESKTOP-3S0SPOS>
2021-05-10 14:51:11 -0500

    SE creo el login

---
+++ ProyectoWebBanda/CapaNegocios/AdminObjeto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoWebBanda.CapaNegocios
+{
+    public class AdminObjeto
+    {
+        public String usuario { get; set; }
+        public String contrasenia { get; set; }
+        public String nombre { get; set; }
+        public String correo { get; set; }
+    }
+}
--- ProyectoWebBanda/CapaUsuarios/Admin/AdminLogin.aspx.cs
+++ ProyectoWebBanda/CapaUsuarios/Admin/AdminLogin.aspx.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using ProyectoWebBanda.CapaNegocios;
+using ProyectoWebBanda.CapaDatos;
 namespace ProyectoWebBanda.CapaUsuarios.Admin
 {
-    public partial class WebForm1 : System.Web.UI.Page
+    public partial  class WebForm1 : System.Web.UI.Page
     {
+       
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -17,6 +19,28 @@ namespace ProyectoWebBanda.CapaUsuarios.Admin
         protected void txtPassword_TextChanged(object sender, EventArgs e)
         {
        
+        }
+
+        protected void btnLog_Click(object sender, EventArgs e)
+        {
+            AdminObjeto obj = new AdminObjeto();
+            obj.usuario = txtUser.Text;
+            obj.contrasenia = txtPassword.Text;
+           // ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('{****************')", true);
+
+            DaoConexion conexion = new DaoConexion();
+
+            Boolean can = conexion.login(obj);
+            if (can)
+            {
+                Response.Redirect("AdminMainMenu.aspx", true);
+            }
+            else
+            {
+
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('usuario no valido')", true);
+
+            }
         }
     }
 }
