ef360c446dee1d91b835d4e85a6f1f5f1cf167fc
Ivan576 <zavalai576@gmail.com>
2021-05-18 10:51:33 -0500

    Diseño de eventos

--- ProyectoWebBanda/CapaUsuarios/Evento.aspx.designer.cs
+++ ProyectoWebBanda/CapaUsuarios/Evento.aspx.designer.cs
@@ -13,5 +13,41 @@ namespace ProyectoWebBanda.CapaUsuarios
 
     public partial class Eventos
     {
+
+        /// <summary>
+        /// Control dateOne.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl dateOne;
+
+        /// <summary>
+        /// Control nameOne.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl nameOne;
+
+        /// <summary>
+        /// Control placeOne.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl placeOne;
+
+        /// <summary>
+        /// Control linkOne.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor linkOne;
     }
 }
